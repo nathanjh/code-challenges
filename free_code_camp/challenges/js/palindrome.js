a0b780a42cc8779c13e19602e49165b15ef0786c
Nathan Herrera <nathanherrera@gmail.com>
2016-09-15 15:53:42 -0700

    Add palindrome test function code to pass tests

--- free_code_camp/challenges/js/palindrome.js
+++ free_code_camp/challenges/js/palindrome.js
@@ -1,3 +1,14 @@
 exports.isPalindrome = function (str) {
-  re = /[\W_]/g;
+  var re = /[\W_]/g;
+  var strForwards = str.replace(re, '').toLowerCase();
+  var strBackwards = '';
+  for (var i = strForwards.length - 1; i >= 0; i--) {
+    strBackwards += strForwards[i];
+  }
+
+  if (strForwards === strBackwards) {
+    return true;
+  } else {
+    return false;
+  }
 };
--- free_code_camp/challenges/js/test/palindrome_spec.js
+++ free_code_camp/challenges/js/test/palindrome_spec.js
@@ -7,3 +7,24 @@ describe('isPalindrome', function () {
     expect(exportFunction.isPalindrome(str)).to.equal(true);
   });
 });
+
+describe('isPalindrome', function () {
+  it('it returns true for "0_0 (: /-\ :) 0-0"', function () {
+    var str = '0_0 (: /-\ :) 0-0';
+    expect(exportFunction.isPalindrome(str)).to.equal(true);
+  });
+});
+
+describe('isPalindrome', function () {
+  it('it returns false for "five|\_/|four"', function () {
+    var str = 'five|\_/|four';
+    expect(exportFunction.isPalindrome(str)).to.equal(false);
+  });
+});
+
+describe('isPalindrome', function () {
+  it('it returns true for "A man, a plan, a canal. Panama"', function () {
+    var str = 'A man, a plan, a canal. Panama';
+    expect(exportFunction.isPalindrome(str)).to.equal(true);
+  });
+});
