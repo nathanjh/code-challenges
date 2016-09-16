601af812ff937a16bbd2f61fde07bfc931029d4f
Nathan Herrera <nathanherrera@gmail.com>
2016-09-16 11:52:43 -0700

    Add tests and function to pass

--- codewars/kata/js/test/isograms_spec.js
+++ codewars/kata/js/test/isograms_spec.js
@@ -6,3 +6,21 @@ describe('isIsogram', function () {
     expect(exportFunction.isIsogram('Dermatoglyphics')).to.equal(true);
   });
 });
+
+describe('isIsogram', function () {
+  it('returns false for "aba"', function () {
+    expect(exportFunction.isIsogram('aba')).to.equal(false);
+  });
+});
+
+describe('isIsogram', function () {
+  it('returns false for "isIsogram"', function () {
+    expect(exportFunction.isIsogram('isIsogram')).to.equal(false);
+  });
+});
+
+describe('isIsogram', function () {
+  it('returns true for ""', function () {
+    expect(exportFunction.isIsogram('')).to.equal(true);
+  });
+});
