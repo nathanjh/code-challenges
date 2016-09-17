44a1e725e268ed20638c7f99417f7e9c6ee918bf
Nathan Herrera <nathanherrera@gmail.com>
2016-09-16 21:03:39 -0700

    Add test code

---
+++ codewars/kata/ruby/test/walk_test_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../walk_test'
+
+describe Walk do
+  it 'can validate a given set of directions' do
+    test_1 = ['n','s','n','s','n','s','n','s','n','s']
+    expect(Walk.is_vaild_walk(test_1)).to eq true
+
+    test_2 = ['w','e','w','e','w','e','w','e','w','e','w','e']
+    expect(Walk.is_vaild_walk(test_2)).to eq false
+
+    test_3 = ['w']
+    expect(Walk.is_vaild_walk(test_3)).to eq false
+
+    test_4 = ['n','n','n','s','n','s','n','s','n','s']
+    expect(Walk.is_vaild_walk(test_4)).to eq false
+  end
+end
