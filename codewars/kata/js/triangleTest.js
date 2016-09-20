70888a58f1dee64a27015b781e206ea21a8c4518
Nathan Herrera <nathanherrera@gmail.com>
2016-09-19 23:42:43 -0700

    Add function code to pass additional tests

--- codewars/kata/js/triangleTest.js
+++ codewars/kata/js/triangleTest.js
@@ -1,8 +1,15 @@
-var exportFunction = require('../triangleTest');
-var expect = require('chai').expect;
-
-describe('triangleType', function () {
-  it('can validate a triangle', function () {
-    expect(exportFunction.triangleType(3, 4, 5)).to.equal(true);
+exports.triangleType = function (a, b, c) {
+  tri = [a, b, c].sort(function (a, b) {
+    return a - b;
   });
-});
+
+  if ((tri[1] - tri[0]) < tri[2] && tri[2] < (tri[1] + tri[0])) {
+    if (((tri[0] * tri[0]) + (tri[1] * tri[1])) === (tri[2] * tri[2])) {
+      return 2;
+    } else if ((tri[2] * tri[2]) > ((tri[0] * tri[0]) + (tri[1] * tri[1]))) { return 3;
+    } else {
+      return 1; }
+  } else {
+    return 0;
+  }
+};
